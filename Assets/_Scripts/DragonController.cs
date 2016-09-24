64cce487f72009c5e82c33f45efc5570e04c06f7
digma89 <digma89@hotmail.com>
2016-09-24 16:39:11 -0400

    add tank move

---
+++ Assets/_Scripts/DragonController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragonController : MonoBehaviour {
+
+    //Private properties
+    private Transform _transform;
+
+    private void _move()
+    {
+        this._transform.position = new Vector2(Mathf.Clamp( Input.mousePosition.x - 320f, -290, 290), -200f);
+    }
+
+	// Use this for initialization
+	void Start () {
+        this._transform = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _move();
+	
+	}
+}
---
+++ Assets/_Scripts/TankController.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankController : MonoBehaviour {
+
+    //Private properties
+    private int _speed;
+    private Transform _transform;
+
+    public int Speed
+    {
+        get
+        {
+            return this._speed;
+        }
+        set
+        {
+            this._speed = value;
+        }
+    }
+
+    //this method moves the background down the screen by _speed px every frame
+    private void _move()
+    {
+        Vector2 newPosition = this._transform.position;
+        newPosition.y -= this._speed;
+        this._transform.position = newPosition;
+
+    }
+
+    private void reset()
+    {
+        this._transform.position = new Vector2(Random.Range(-280, 288), 270f);
+    }
+
+    //this method checks to see if the game object meets the top-border of screen
+    private void _checkBounds()
+    {
+        if (this.transform.position.y <= -270f)
+        {
+            this._reset();
+        }
+    }
+
+    //this method resets the game object to the original position
+    private void _reset()
+    {
+        this._transform.position = new Vector2(0f, 480f);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        this._transform = this.GetComponent<Transform>();
+        this.Speed = 5;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._move();
+        this._checkBounds();
+
+    }
+}
